02d9822fc719670c2ab4a1a26496b9a01ce7cfae
Giulio Falco <gfalco58@gmail.com>
2022-03-12 21:19:35 +0100

    ritorno a sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,11 +81,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
         
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite',
+       #  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       # 'NAME': 'mysite',
        # 'USER': 'giulio',
        # 'PASSWORD': 'benoni58',
        # 'PORT': '5432',
+        "ENGINE" : "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
 
