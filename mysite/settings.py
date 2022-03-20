002e7a0dad4b920db7a7a85d6d1a4705a5093451
Giulio Falco <gfalco58@gmail.com>
2022-03-20 09:51:11 +0100

    tentativo num 2 lasciato solo NAME

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
         
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
-        #'USER': 'giulio',
+        'USER': 'giulio',
         #'PASSWORD': 'benoni58',
         #'HOST': '127.0.0.1',
         #'PORT': '5432',
