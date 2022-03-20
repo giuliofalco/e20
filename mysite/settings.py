286bad6ffdacae1bbde8dd639088b487d4f30fb7
Giulio Falco <gfalco58@gmail.com>
2022-03-20 09:52:50 +0100

    tentativo num 2 lasciato solo NAME

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
         'USER': 'giulio',
-        #'PASSWORD': 'benoni58',
+        'PASSWORD': 'benoni58',
         #'HOST': '127.0.0.1',
         #'PORT': '5432',
         # 'PORT': '5432',
