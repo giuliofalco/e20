86e027182b15f6179d404b7c6ca2affbed40d0a0
Giulio Falco <gfalco58@gmail.com>
2022-03-20 18:46:24 +0100

    terzo tentativo postgres heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,9 +81,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-db_from_env=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 DATABASES = {
         
     'default': {
@@ -98,7 +95,8 @@ DATABASES = {
         # "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
-
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
