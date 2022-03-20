1930f363e9986901dd7808610d23fb82232e4d44
Giulio Falco <gfalco58@gmail.com>
2022-03-20 14:53:13 +0100

    secondo tentativo postgres heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,12 +83,12 @@ DATABASES = {
         
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mysite',
-        'USER': 'giulio',
-        'PASSWORD': 'benoni58',
-        #'HOST': '127.0.0.1',
-        #'PORT': '5432',
-        # 'PORT': '5432',
+        #'NAME': 'mysite',
+        #'USER': 'giulio',
+        #'PASSWORD': 'benoni58',
+        'HOST': 'DATABASE_URL',
+        'PORT': '5432',
+       
         # "ENGINE" : "django.db.backends.sqlite3",
         # "NAME": os.path.join(BASE_DIR, "db.sqlite3")
     }
