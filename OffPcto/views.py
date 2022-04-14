9b259c9ec31a404d8087d657f8740ad37710b853
Giulio Falco <gfalco58@gmail.com>
2022-04-14 23:30:51 +0200

    non trova template index senza sottodirectory

--- OffPcto/views.py
+++ OffPcto/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from OffPcto.models import *
 
 def index(request):
-    return render(request,'"OffPcto"/index.html',{})
+    return render(request,'index.html',{})
     
 def tutor(request):
      elenco = Tutor.objects.all()
      context = {'object_list':elenco}
-     return render(request,"OffPcto/tutor.html",context)
+     return render(request,"tutor.html",context)
 
 def aziende(request):
     elenco = Aziende.objects.all()
     context = {'object_list': elenco}
-    return render(request,"OffPcto/aziende.html",context)
+    return render(request,"aziende.html",context)
     
