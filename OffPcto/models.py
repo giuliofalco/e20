aee3d888f579e38822a3e2a00a4dac955cb5759c
Giulio Falco <gfalco58@gmail.com>
2022-04-15 11:26:10 +0200

    introdotto lo score x aziende

--- OffPcto/models.py
+++ OffPcto/models.py
@@ -11,12 +11,13 @@ class Tutor(models.Model):
 class Aziende(models.Model):
     partita_iva = models.CharField(max_length=80,null=True)
     ragione_sociale = models.CharField(max_length=200)
-    tutor_referente_azienda = models.CharField(max_length=200,null=True )
+    tutor_referente_azienda = models.CharField(max_length=200,null=True)
     sede_comune = models.CharField(max_length=200,null=True)
     sede_provincia = models.CharField(max_length=20,null=True)
     telefono = models.CharField(max_length=200,null=True)
     email = models.CharField(max_length=100)
     settore = models.CharField(max_length=200,null=True)
+    score = models.IntegerField(default=0)
     
     def __str__(self):
        return(self.ragione_sociale)
