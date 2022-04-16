ab5327d3f991f53758f6ac9554fac7853723ca1d
Giulio Falco <gfalco58@gmail.com>
2022-04-16 13:37:00 +0200

    aggiunto classi in tutor

--- OffPcto/dati/carica_dati.py
+++ OffPcto/dati/carica_dati.py
@@ -6,12 +6,21 @@ from oop_pcto import *
 #object = TutorImportPg('tutor.csv','mysite')
 #object.aggiornaDb()
 
-object = AziendeImportPg('aziende.tsv','mysite')
-object.aggiornaDb()
+#for item in object.source.dati:
+#    print(item)
+
+#object = AziendeImportPg('aziende.tsv','mysite')
+#object.aggiornaDb()
+
+#for item in object.source.dati:
+#    print(item)
 
 # AGGIORNAMENTO DATABASE SQLITE
 
 #object = AziendeImport('aziende.tsv','pcto.db')
 #object.aggiornaDb()
+
+object = Classi_tutorPg('classi_tutor','mysite')
+object.aggiornaDb()
 
 print ("Done")
--- OffPcto/models.py
+++ OffPcto/models.py
@@ -4,6 +4,7 @@ class Tutor(models.Model):
     nome = models.CharField(max_length=200)
     cognome = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
+    classi = models.CharField(max_length=200,null=True,blank=True)
     
     def __str__(self):
        return(f"{self.cognome} {self.nome}")
