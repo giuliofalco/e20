d075f41146952d61f4ecd725be47d8c31202eb75
Giulio Falco <gfalco58@gmail.com>
2022-04-16 13:52:06 +0200

    aggiunto classi ad admin e template tutor

--- OffPcto/admin.py
+++ OffPcto/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import Tutor, Aziende
 
-admin.site.register(Tutor)
+#admin.site.register(Tutor)
 #admin.site.register(Aziende)
+
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin);
+    list_display = ['cognome','nome','email','classi']
 
 @admin.register(Aziende)
 class AziendeAdmin(admin.ModelAdmin):
