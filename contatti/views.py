0536fa7e1e0f31f6140e2c2d356ef9bc0edba91c
Giulio Falco <gfalco58@gmail.com>
2023-01-03 12:35:52 +0100

    added pagination to aziende.html

--- contatti/views.py
+++ contatti/views.py
@@ -3,6 +3,7 @@ from contatti.models import *
 from .filters import AziendeFilter
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 def index(request):
     
@@ -14,25 +15,26 @@ def aziende(request):
     # fornisce l'elenco della aziende presenti nel database
     elenco = Aziende.objects.all()
    
-    # numero_aziende = len(elenco)
+    numero_aziende = len(elenco)
     myfilter = AziendeFilter(request.GET,queryset=elenco)
     elenco = myfilter.qs
-    context = {'elenco':elenco,'myfilter':myfilter,}
+    
 
-   # pag = Paginator(elenco,20)
-   # totpagine = pag.num_pages
-   # numpag = request.GET.get('pagina',1)
-   # pagelist = range(int(numpag)+1,totpagine+1)
-   # pagelist0 = range(1,int(numpag))
-   # pagina = pag.page(numpag)
+    pag = Paginator(elenco,12)
+    totpagine = pag.num_pages
+    numpag = request.GET.get('pagina',1)
+    pagelist = range(int(numpag)+1,totpagine+1)
+    pagelist0 = range(1,int(numpag))
+    pagina = pag.page(numpag)
 
-   # context = {'object_list': pagina,           # la pagina con i dati da visualizzare
-   #            'myfilter':myfilter,
-   #            'totpagine': totpagine,
-   #            'numpag' : numpag,
-   #            'pagelist': pagelist, 
-   #            'pagelist0': pagelist0,
-   #            'numero_aziende' : numero_aziende
-   #           }
-   # context['user'] = visualizza_utente(request)
+    #context = {'elenco':elenco,'myfilter':myfilter,}
+    context = {'object_list': pagina,           # la pagina con i dati da visualizzare
+               'myfilter':myfilter,
+               'totpagine': totpagine,
+               'numpag' : numpag,
+               'pagelist': pagelist, 
+               'pagelist0': pagelist0,
+               'numero_aziende' : numero_aziende
+              }
+    # context['user'] = visualizza_utente(request)
     return render(request,"contatti/aziende.html",context)
