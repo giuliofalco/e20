44db1a14e0ed6c122c146b3ee921f9505f99f927
Giulio Falco <gfalco58@gmail.com>
2024-05-28 18:26:09 +0200

    aggiornato filtro sui contatti con la provincia ridimensionato il widget

--- contatti/filters.py
+++ contatti/filters.py
@@ -17,12 +17,17 @@ class ContattiFilter(django_filters.FilterSet):
   
     azienda = CharFilter(field_name="azienda__nome",lookup_expr="icontains")
     citta = CharFilter(field_name="azienda__citta",lookup_expr="icontains")
+    provincia = CharFilter(field_name = "azienda__provincia",lookup_expr="icontains")
     note = CharFilter(field_name="note",lookup_expr="icontains")
     da_chiamare = ChoiceFilter(field_name="da_chiamare", choices=DA_CHIAMARE, method='filtro_attivo')
 
     class Meta: 
         model=Contatti
         fields=['azienda','citta','note','da_chiamare']
+
+    def __init__(self, *args, **kwargs):
+        super(ContattiFilter, self).__init__(*args, **kwargs)
+        self.filters['provincia'].field.widget.attrs.update({'class': 'custom-province-field'})
 
     def filtro_attivo(self, queryset, name, value):
         if value == '1':
