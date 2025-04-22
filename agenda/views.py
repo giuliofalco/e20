9d3ee71a70311a5572cc5b010b81394a85764ac6
Giulio Falco <gfalco58@gmail.com>
2025-04-22 19:29:00 +0200

    risistemato day_editor x cooky

--- agenda/views.py
+++ agenda/views.py
@@ -91,7 +91,12 @@ def day_editor(request, year, month, day):
         form = DayEntryForm(request.POST, instance=day_entry)
         if form.is_valid():
             form.save()
-            response = redirect('calendar_view')
+            request.set_cookie(
+                key=day_entry.date.strftime('%Y-%m-%d'),
+                value=day_entry.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+                max_age=60 * 60 * 24 * 365  # Cookie valido per 1 anno
+            )
+            return redirect('calendar_view')
     else:
         form = DayEntryForm(instance=day_entry)
         context = {'form': form, 'entry_date': entry_date, 'mese':MESI[month-1],
@@ -100,14 +105,13 @@ def day_editor(request, year, month, day):
                    'prev_year': prev_date.year,'next_year': next_date.year, 
                    'weekday' : weekday, 
         }
-        response = render(request, 'agenda/day_editor.html', context )
-    response.set_cookie(
-        key=day_entry.date.strftime('%Y-%m-%d'),
-        value=day_entry.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
-        max_age=60 * 60 * 24 * 365  # Cookie valido per 1 anno
-    )
-    
-    return response
+        request.set_cookie(
+                key=day_entry.date.strftime('%Y-%m-%d'),
+                value=day_entry.updated_at.strftime('%Y-%m-%d %H:%M:%S'),
+                max_age=60 * 60 * 24 * 365  # Cookie valido per 1 anno
+        )
+        return render(request, 'agenda/day_editor.html', context )
+
 
 
 @xframe_options_exempt
