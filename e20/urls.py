fb4e287f3eaaea58b3ff8dcaef05b6d1958c8228
Giulio Falco <gfalco58@gmail.com>
2025-10-12 22:21:23 +0200

    in primo piano agenda

--- e20/urls.py
+++ e20/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('contatti/',include('contatti.urls')),
     path('agenda/',include('agenda.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),  # solo se usi l’uploader
-    path('', RedirectView.as_view(url='diario/', permanent=True)),
+    path('', RedirectView.as_view(url='agenda/', permanent=False)),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
